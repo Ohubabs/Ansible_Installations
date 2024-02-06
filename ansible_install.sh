5a7eec2d6818f90cb86467809fef0586a58fe23c
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-02-06 10:09:40 -0800

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo hostnamectl set-hostname ansible
 sudo timedatectl set-timezone American/Los_Angeles
-sudo useradd ansible
+sudo adduser ansible
 echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
